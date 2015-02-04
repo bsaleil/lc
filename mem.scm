4650231818cfb414791f39e439a1fb7ddbfbe49b
bsaleil <baptiste.saleil@gmail.com>
2015-02-04 12:43:41 -0500

    Add symbol to GC

--- mem.scm
+++ mem.scm
@@ -108,6 +108,7 @@
         ((eq? stag STAG_MOBJECT)   (println "MOBJECT"))
         ((eq? stag STAG_VECTOR)    (println "VECTOR"))
         ((eq? stag STAG_STRING)    (println "STRING"))
+        ((eq? stag STAG_SYMBOL)    (println "SYMBOL"))
         ((eq? stag STAG_CCTABLE)   (println "CCTABLE"))
         (else
           (print "Unknown stag ")
@@ -263,8 +264,8 @@
                     ((= s STAG_VECTOR)
                       (let ((sc (scan-vector scan copy h s l)))
                         (scan-references (car sc) (cdr sc))))
-                    ;; CC-table & String
-                    ((or (= s STAG_CCTABLE) (= s STAG_STRING))
+                    ;; CC-table & String & Symbol
+                    ((or (= s STAG_CCTABLE) (= s STAG_STRING) (= s STAG_SYMBOL))
                       ;; Nothing to do
                       (scan-references (+ scan (* 8 l)) copy))
                     ;; MObject
@@ -491,7 +492,8 @@
 ;               ;; stag
 ;               (cond ;; CCTABLE & STRING
 ;                     ((or (= s STAG_CCTABLE)
-;                          (= s STAG_STRING))
+;                          (= s STAG_STRING)
+;                          (= s STAG_SYMBOL))
 ;                         ;; Nothing to do
 ;                         (check-heap (+ scan (* 8 l)) copy))
 ;                     ;; PROCEDURE
--- test.scm
+++ test.scm
@@ -4,37 +4,5 @@
 ;; Test quoted vector ex. '#(1 2 "Hello" ...)
 ;; Test set-car, set-cdr + Check err
 ;; Test bind define
-
-; (define (bar n m)
-;   (begin ($$putchar ($+ n m))
-;          (foo 55)))
-
-; (define (foo n)
-;   (begin ($$putchar n)
-;          (bar 55 2)))
-
-; (foo 55)
-
-(define (foo n)
-  10)
-
-(define (make-string size . init)
-    (foo 20))
-
-
-(make-string 4)
-
-
-
-
-; (define (string-append-h strings)
-;   (cond ((null? strings) "")
-;         ((null? (cdr strings)) (car strings))
-;         (else
-;          (string-append-h
-;           (cons (string-append-two (car strings) (cadr strings))
-;                 (cddr strings))))))
-
-
-; (foo 10)
-; (bar 10)
+;; Test appel terminal
+;; Faire egalites conformes (= eq eqv equal)
