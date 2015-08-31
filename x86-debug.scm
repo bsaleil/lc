f51c36e9903ab900def9248af35ada202ae2c491
bsaleil <baptiste.saleil@gmail.com>
2015-08-31 18:35:17 -0400

    Add help to breakpoint commands

--- x86-debug.scm
+++ x86-debug.scm
@@ -8,7 +8,7 @@
 
 ;; Gen call to breakpoint label
 (define (gen-breakpoint cgc)
-  
+
   (push-pop-regs
      cgc
      all-regs
@@ -23,16 +23,16 @@
 
 ;; Give a cmd to user to print registers or stack
 (c-define (break_point sp) (long) void "break_point" ""
-  
+
   (let ((reg-names '("R15" "R14" "R13" "R12" "R11" "R10" " R9" " R8"
                      "RDI" "RSI" "RBP" "RDX" "RCX" "RBX" "RAX" "RSP")))
-  
+
   (define (get-regs regs offset)
     (if (null? regs)
       '()
       (cons (cons (car regs) (get-i64 (+ sp offset)))
             (get-regs (cdr regs) (+ offset 8)))))
-  
+
   (define (println-slot slot)
     (let ((str (number->string slot)))
       (print slot (make-string (- 19 (string-length str)) #\space)))
@@ -44,7 +44,7 @@
             ((= slot ENCODING_EOF)  (print "eof"))
             ((= tag 2) (print (encoding-obj slot)))))
     (newline))
-  
+
   (define (print-stack n regs)
     (cond ((= n 0))
           ((= n 1)
@@ -55,23 +55,31 @@
              (print-stack (- n 1) regs)
              (print   "       | ") (println-slot (get-i64 (+ (* 8 (- n 1)) (cdr (assoc "RSP" regs)))))
              (println "       +--------------------+--------------------+"))))
-  
+
   (define (print-regs regs)
     (if (not (null? regs))
       (begin (print-regs (cdr regs))
              (print-reg  (car regs)))))
-  
+
   (define (print-reg reg)
     (println (car reg) " = " (cdr reg)))
-    
+
   (let ((regs (get-regs reg-names 0)))
-    
+
     (define (run)
       (print ">")
       (let ((r (read-line)))
         (set! r (string-upcase r))
         (let ((reg (assoc r regs)))
-          (cond ;; Print one reg
+          (cond ;; Print help
+                ((string=? r "HELP")
+                   (println "Breakpoint commands:")
+                   (println "   r*      | print register (rax, rbx, ...)")
+                   (println "   regs    | print registers")
+                   (println "   stack   | print 5 stack slots")
+                   (println "   stack n | print n stack slots")
+                   (println "   next    | continue"))
+                ;; Print one reg
                 ((assoc r regs) => (lambda (r) (print-reg r)))
                 ;; Print all regs
                 ((string=? r "REGS")  (print-regs regs))
@@ -93,7 +101,7 @@
                 (else (println "Unknown command")))
           (if (not (string=? r "NEXT"))
             (run)))))
-    
+
     (println "\033[1;31m⚫ Breakpoint\033[0m")
     (run))))
 
@@ -115,7 +123,7 @@
        (x86-pop  cgc (x86-rsp)) ;; restore unaligned stack-pointer
        )))
 
-;; Get all regs vals from 'regs-pos' list    
+;; Get all regs vals from 'regs-pos' list
 (define (get-regs-val regs-pos sp)
   (if (null? regs-pos)
       '()
@@ -127,7 +135,7 @@
       '()
       (begin (println "   " (string-append (car regs-name) " : " (number->string (car regs-val))))
              (pp-regs (cdr regs-name) (cdr regs-val)))))
-        
+
 ;; Get registers values from stack and pretty print
 ;; NOTE : hardcoded registers positions in stack to match 'all-regs' list
 (c-define (dump-regs sp) (long) void "dump_regs" ""
@@ -135,7 +143,7 @@
          (regs-name '("RSP" "RAX" "RBX" "RCX" "RDX" "RBP" "RSI" "RDI"
                       "R8 "  "R9 "  "R10" "R11" "R12" "R13" "R14" "R15"))
          (regs-val (get-regs-val regs-pos sp)))
-     
+
     (pp-regs regs-name regs-val)))
 
 ;;-----------------------------------------------------------------------------
