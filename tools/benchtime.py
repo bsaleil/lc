2319aa1338e31b8d5bda0e9e2ed94e674f8c12e0
bsaleil <baptiste.saleil@umontreal.ca>
2015-10-30 15:56:21 -0400

    Impro

--- tools/benchtime.py
+++ tools/benchtime.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 
-import sys # TODO
+import sys
 import os
 import glob
 import subprocess
 from pylab import *
 from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import rc
+from matplotlib.ticker import FuncFormatter
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
 
 # -------------------------
 # Constants
@@ -16,8 +21,9 @@ LC_PATH     = SCRIPT_PATH + '../'                               # Compiler path
 LC_EXEC     = 'lazy-comp'                                       # Compiler exec name
 PDF_OUTPUT  = SCRIPT_PATH + 'times.pdf'                         # PDF output file
 BENCH_PATH  = LC_PATH + 'benchmarks/*.scm'                      # Benchmarks path
-BAR_COLORS  = ["#222222", "#666666", "#AAAAAA", "#EEEEEE"]      # Bar colors
+BAR_COLORS  = ["#000000", "#444444", "#888888", "#CCCCCC"]      # Bar colors
 ITERS       = 10                                                # Number of iterations >=4 (we remove first, min and max)
+FONT_SIZE   = 9                                                 # Font size used to generate pdf using latex (must match the paper font size)
 
 # -------------------------
 # Config
@@ -56,6 +62,10 @@ for file in files:
     idx+=1
     print('(' + str(idx) + '/' + str(len(files)) + ') ' + file);
 
+    # Exec without versioning
+    print('\t* No versioning...')
+    time_nv = getTime(file,['--disable-entry-points', '--disable-return-points','--max-versions 0']);
+
     # Exec with versioning only
     print('\t* Versioning only...')
     time_v = getTime(file,['--disable-entry-points', '--disable-return-points']);
@@ -72,7 +82,7 @@ for file in files:
     print('\t* Versioning + entry points + return points...')
     time_ver = getTime(file,[]);
 
-    TIME.append([file,time_v,time_ve,time_vr,time_ver])
+    TIME.append([file,time_nv,time_v,time_ve,time_vr,time_ver])
 
 # -------------------------
 # Draw graph
@@ -89,67 +99,91 @@ print('Draw graph...')
 # Graph config
 nb_items = len(files) + 1                     # Number of times to draw (+1 for arithmetic mean)
 bar_width = 0.8                               # Define widht of a single bar
-matplotlib.rcParams.update({'font.size': 19}) # Set font size of all elements of the graph
-fig = plt.figure('',figsize=(22,9))           # Create new figure
+matplotlib.rcParams.update({'font.size': FONT_SIZE}) # Set font size of all elements of the graph
+#fig = plt.figure('',figsize=(22,9))           # Create new figure
+fig = plt.figure('',figsize=(8,3.4))
 plt.title('Execution time')                   # Set title
 gca().get_xaxis().set_visible(False)          # Hide x values
 
-ylim(0,200)                                   # Set y scale from 0 to 200
+ylim(0,120)                                   # Set y scale from 0 to 120
+xlim(0, nb_items*5)                           # Set x scale from 0 to nb_items*5
 fig.subplots_adjust(bottom=0.4)               # Give more space at bottom for benchmark names
-plot([0,nb_items*4],[100,100], color="#CCCCCC", linewidth=1) # Draw y=100 line
+plot([0,nb_items*5],[100,100], color="#CCCCCC", linewidth=1) # Draw y=100 line
 
 # Convert times to % times
 TIMEP = []
 for t in TIME:
     file = t[0]
-    time_v = t[1]
-    time_ve = t[2]
-    time_vr = t[3]
-    time_ver = t[4]
+    time_nv = t[1]
+    time_v = t[2]
+    time_ve = t[3]
+    time_vr = t[4]
+    time_ver = t[5]
     # Compute % values relative to time with versioning only
-    timep_v   = 100
-    timep_ve  = (100*time_ve)  / time_v
-    timep_vr  = (100*time_vr)  / time_v
-    timep_ver = (100*time_ver) / time_v
+    timep_nv  = 100
+    timep_v   = (100*time_v)   / time_nv
+    timep_ve  = (100*time_ve)  / time_nv
+    timep_vr  = (100*time_vr)  / time_nv
+    timep_ver = (100*time_ver) / time_nv
 
-    TIMEP.append([file,timep_v,timep_ve,timep_vr,timep_ver])
+    TIMEP.append([file,timep_nv,timep_v,timep_ve,timep_vr,timep_ver])
 
 # Sort by timep_ver
-TIMEP.sort(key=lambda x: x[4])
+TIMEP.sort(key=lambda x: x[5])
 
 # Add arithmetic mean values
 name = 'arith. mean'
-timep_v   = 100
-timep_ve  = sum(list(map(lambda x: x[2], TIMEP)))/len(files)
-timep_vr  = sum(list(map(lambda x: x[3], TIMEP)))/len(files)
-timep_ver = sum(list(map(lambda x: x[4], TIMEP)))/len(files)
-TIMEP.append([name,timep_v,timep_ve,timep_vr,timep_ver])
+time_nv   = 100
+timep_v   = sum(list(map(lambda x: x[2], TIMEP)))/len(files)
+timep_ve  = sum(list(map(lambda x: x[3], TIMEP)))/len(files)
+timep_vr  = sum(list(map(lambda x: x[4], TIMEP)))/len(files)
+timep_ver = sum(list(map(lambda x: x[5], TIMEP)))/len(files)
+TIMEP.append([name,time_nv,timep_v,timep_ve,timep_vr,timep_ver])
+
+# DRAW V
+X = np.arange(0,nb_items*5,5) # [0,5,10,..]
+drawBar(TIMEP,2,0,'Vers.')
 
 # DRAW VE
-X = np.arange(0,nb_items*4,4)   # [0,4,8,..]
-drawBar(TIMEP,2,0,'Versioning + Entry')
+X = np.arange(1,nb_items*5,5)   # [1,6,11,..]
+drawBar(TIMEP,3,1,'Vers. + Entry')
 
 # DRAW VR
-X = np.arange(1,nb_items*4+1,4) # [1,5,9,..]
-drawBar(TIMEP,3,1,'Versioning + Return')
+X = np.arange(2,nb_items*5,5) # [2,7,12,..]
+drawBar(TIMEP,4,2,'Vers. + Return')
 
 # DRAW VER
-X = np.arange(2,nb_items*4+2,4) # [2,6,10,..]
-drawBar(TIMEP,4,2,'Versioning + Entry + Return')
+X = np.arange(3,nb_items*5,5) # [3,6,10,..]
+drawBar(TIMEP,5,3,'Vers. + Entry + Return')
 
 # DRAW BENCHMARK NAMES
-i = 1
+i = 0
 for time in TIMEP[:-1]:
-    text(i+bar_width/2, -10, os.path.basename(time[0])[:-4], rotation=90, ha='center', va='top')
-    i+=4
-text(i+bar_width/2, -10,TIMEP[-1][0], rotation=90, ha='center', va='top') # arithmetic mean time (last one)
+    text(i+2-((1-bar_width)/2), -10, os.path.basename(time[0])[:-4], rotation=90, ha='center', va='top')
+    i+=5
+text(i+2-((1-bar_width)/2), -10,TIMEP[-1][0], rotation=90, ha='center', va='top') # arithmetic mean time (last one)
 
-legend(bbox_to_anchor=(0., 0., 1., -0.55), prop={'size':19}, ncol=3, mode="expand", borderaxespad=0.)
+legend(bbox_to_anchor=(0., 0., 1., -0.55), prop={'size':FONT_SIZE}, ncol=4, mode="expand", borderaxespad=0.)
+
+
+def to_percent(y, position):
+    s = str(int(y))
+    # The percent symbol needs escaping in latex
+    if matplotlib.rcParams['text.usetex'] is True:
+        return s + r'$\%$'
+    else:
+        return s + '%'
+
+formatter = FuncFormatter(to_percent)
+plt.gca().yaxis.set_major_formatter(formatter)
 
 ## SAVE/SHOW GRAPH
 pdf = PdfPages(PDF_OUTPUT)
 pdf.savefig(fig)
 pdf.close()
+#plt.show()
+#plt.savefig('tex_demo')
+#plt.savefig('tex_demo')
 #plt.show()
 
 print('Saved to ' + PDF_OUTPUT)
