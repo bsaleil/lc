bac4decf3a601543a99a70905971994c716ebbf2
bsaleil <baptiste.saleil@gmail.com>
2016-08-28 12:24:44 -0400

    Fix bash syntax in copy-with-declare

--- copy-with-declare.sh
+++ copy-with-declare.sh
@@ -3,11 +3,13 @@
 echo "(declare (standard-bindings) (extended-bindings) (not inline-primitives) (block) (not safe))" > "$2"
 
 # Library
-if [ "$3" == "lib" ]; then
+if [[ "$3" == "lib" ]]
+then
   cat ./lib.scm >> "$2"
 fi
 
 # Code
-if [ "$1" != "" ]; then
+if [[ "$1" != "" ]]
+then
   cat "$1" >> "$2"
 fi
