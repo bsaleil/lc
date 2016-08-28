c851cc9481545fa4ef3b0a6b4f149f905007e349
bsaleil <baptiste.saleil@gmail.com>
2016-08-28 12:50:00 -0400

    Update failing ut to match scheme spec

--- unit-tests/pp.scm
+++ unit-tests/pp.scm
@@ -40,8 +40,6 @@
 
 (pp '())
 
-(pp (lambda (x) (* x x)))
-
 (pp (cons 10 20))
 (pp (cons 10 (cons 20 '())))
 (pp (cons 99 (cons #f (cons '() '()))))
@@ -75,7 +73,6 @@
 ;#f
 ;#f
 ;()
-;#<procedure #4>
 ;(10 . 20)
 ;(10 20)
 ;(99 #f ())
