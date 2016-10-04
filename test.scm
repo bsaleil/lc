984228e9d07e83adcf4cd4da2392431470531b10
bsaleil <baptiste.saleil@gmail.com>
2016-10-04 12:06:18 -0400

    minor changes (notes)

--- test.scm
+++ test.scm
@@ -5,47 +5,25 @@
 (for-each (lambda (n) (print n)) 100)
 
 
+;; ->
+;; * Merge regalloc
+;; * Merge max versions
+;; * overflow
+;; * add bound tests
 
-
-
-
-
-
-
-
-
-;; Allocation des procédures cst:
-;; à tous les drop-cst-value, et gen-closure*:
-;; -> uiliser simplement un mov de la globale
-;; -> conserver une table fn-id -> permanent object
 
 ;; NEXT:
 ;; * check cc-key
 ;; * utiliser un systeme pour les globales non mutables compatible avec le nouvel cst vers.
-;; * use global-fn for cst fn ? (permanent objects?)
-;; * merge code
 ;; * return value (type cr)
 
-;; TODO: mlc-lambda, détecter les fn const, et les ajouter au contexte sans générer de code
 ;; TODO: quand on récupère l'emplacement d'une variable, regarder les slots pour trouver la meilleure loc (cst > reg > mem)
 ;; TODO: #<ctx-tclo #3 sym: closure mem-allocated?: #t is-cst: (lambda () ($$atom 1)) cst: #f fn-num: 0>
 ;;       pourquoi l'ast dans is-cst?
-;; TODO: cas spéciaux comme not, eof?, ... si l'opérande est constante, on connait le résultat
-;;       pour le moment traité dans codegen, mais movs inutiles, et on perd l'info cst
 ;; TODO: merge de regalloc
 ;; TODO: merge de version
 ;; TODO: jitter le alloc-rt pour ne pas générer de code si la taille ne nécessite pas un still
-;; TODO: pour (dé)sactiver les cst interprocédural:
-;;   - Ajouter le support pour apply-moves et ctx-get-call-args-moves
-;;   - Dans ctx-init-fn, modifier la stack pour enlever la cst
 ;; TODO: ajouter le support des constantes dans les globales non mutables
-
-
-;(define (bar a b c d)
-;  (println a b c d))
-;
-;(apply bar 22)
-
 
 ;; Liveness: terminer le travail
 ;; Letrec: attention aux lates !function
@@ -57,11 +35,3 @@
 ;; TODO: optimization: pour un pt entrée:
 ;;       * si on génère un pt entrée dont la 1ere instruction est un jump,
 ;;       * on peut patcher le pt d'entrée pour sauter directement au bon endroit
-;; TODO: utiliser les informations de type pour:
-;;       * eq?
-;;       * equal?
-;;       * eqv?
-;; TODO: inliner les primitives + inliner en fonction des types pour:
-;;       * eq?
-;;       * equal?
-;;       * eqv?
