beff5633dd0ab5603e8561ff39d847dace9a0beb
bsaleil <baptiste.saleil@gmail.com>
2017-05-23 11:38:03 -0400

    remove unused benchmark folder

--- benchmarks/fib.scm
+++
@@ -1,9 +0,0 @@
-;;; FIB -- A classic benchmark
-
-(define (fib n)
-  (if (< n 2)
-    n
-    (+ (fib (- n 1))
-       (fib (- n 2)))))
-
-(apply fib (list 35))
--- benchmarks/fibfp.scm
+++
@@ -1,9 +0,0 @@
-;;; FIBFP -- Computes fib(35) using floating point
-
-(define (fibfp n)
-  (if (FLOAT< n 2.)
-    n
-    (FLOAT+ (fibfp (FLOAT- n 1.))
-            (fibfp (FLOAT- n 2.)))))
-
-(apply fibfp (list 35.))
